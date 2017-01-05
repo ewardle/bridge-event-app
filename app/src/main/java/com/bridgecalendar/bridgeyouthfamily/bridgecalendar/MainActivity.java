dd407b1f272b9ad09b9ab8f255c0bdf61c2be4a7
aryad <aryad999@gmail.com>
2017-01-04 18:05:06 -0800

    Initial Commit

---
+++ app/src/main/java/com/bridgecalendar/bridgeyouthfamily/bridgecalendar/MainActivity.java
@@ -0,0 +1,42 @@
+package com.bridgecalendar.bridgeyouthfamily.bridgecalendar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Arya on 1/4/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
