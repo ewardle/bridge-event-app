9e190b829847a2a3af858f7235170a106e9961d0
aryad <aryad999@gmail.com>
2017-01-04 18:13:26 -0800

    Added MonthCalendarFragment

---
+++ app/src/main/java/com/bridgecalendar/bridgeyouthfamily/bridgecalendar/MonthCalendarFragment.java
@@ -0,0 +1,10 @@
+package com.bridgecalendar.bridgeyouthfamily.bridgecalendar;
+
+import android.app.Fragment;
+
+/**
+ * Created by Arya on 1/4/2017.
+ */
+
+public class MonthCalendarFragment extends Fragment {
+}
